b0e6d3a178795ca6aef766ef714e8446e529b4ff
Joloso01 <jbermejop01@elpuig.xeill.net>
2021-02-01 00:20:21 +0100

    Initial Commit

--- src/mp5/uf2/Main.java
+++ src/mp5/uf2/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mon!");
         System.out.println("clonación1");
+        System.out.println("Rama1");
     }
 }
